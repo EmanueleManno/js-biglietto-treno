b17e52c26f17f86d7bb70c647a1cea8058c4ba8c
EmanueleManno <mannus1994@gmail.com>
2023-05-26 16:33:30 +0200

    Push Pre-Tutoring

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //TESTIAMO SE FUNZIONA
 console.log('JS OK');
 
-//RECUPERO L'ELEMENTO NELLA PAGINA
+//RECUPERO GLI ELEMENTI NELLA PAGINA
 const finalprice = document.getElementById('final-price');
 console.log(finalprice);
+const discount = document.getElementById('discount');
+console.log(discount);
+const fullprice = document.getElementById('full-price');
+console.log(full-price);
 
 //CHIEDIAMO ALL'UTENTE IL NUMERO DI CHILOMETRI CHE VUOLE PERCORRERE
 const kilometers = prompt('Quanti Km desidera fare caro ospite?');
@@ -25,20 +29,48 @@ console.log(calcdecimalprice);
 const textprice = "Il prezzo che lei deve sganciare senza aver diritto a sconti è € "
 
 //SCRIVO ALL'UTENTE IL PREZZO SENZA SCONTI
-finalprice.innerText = textprice + calcdecimalprice;
+fullprice.innerText = textprice + calcdecimalprice;
 
 //SE L'ETA' E' MINORE DI 18 ANNI CALCOLO SCONTO E PREZZO SCONTATO
-if (age < 18); {
+if (age < 18); 
+{
     const minordiscount = ((calcdecimalprice*20)/100);
     console.log(minordiscount);
     const calcminorprice = calcdecimalprice - minordiscount;
     console.log(calcminorprice);
 
-    //PREPARO IL TESTO DA SCRIVERE PER IL PREZZO SE MINORENNE
+    //PREPARO IL TESTO DA SCRIVERE PER LO SCONTO SE MINORENNE
+    const textminordiscount = "Lo sconto a lei riservato da minorenne è € " 
+
+    //PREPARO IL TESTO DA SCRIVERE PER IL PREZZO SCONTATO SE MINORENNE
     const textminorprice = "Il prezzo che lei deve sganciare da minorenne è € " 
-    
-    //SCRIVO ALL'UTENTE IL PREZZO SENZA SCONTI
+
+    //SCRIVO ALL'UTENTE IL LO SCONTO
+    discount.innerText = textminordiscount + minordiscount;
+
+    //SCRIVO ALL'UTENTE IL PREZZO SCONTATO
     finalprice.innerText = textminorprice + calcminorprice;
+}
+
+//SE L'ETA' E' MAGGIORE O UGUALE A 65 ANNI CALCOLO SCONTO E PREZZO SCONTATO
+else if (age >= 65); 
+{
+    const majordiscount = ((calcdecimalprice*40)/100);
+    console.log(majordiscount);
+    const calcmajorprice = calcdecimalprice - majordiscount;
+    console.log(calcmajorprice);
+
+    //PREPARO IL TESTO DA SCRIVERE PER LO SCONTO SE OVER 65
+    const textmajordiscount = "Lo sconto a lei riservato da over 65 è € " 
+
+    //PREPARO IL TESTO DA SCRIVERE PER IL PREZZO SCONTATO SE OVER 65
+    const textmajorprice = "Il prezzo che lei deve sganciare da over 65 è € " 
+
+    //SCRIVO ALL'UTENTE IL LO SCONTO
+    discount.innerText = textmajordiscount + majordiscount;
+
+    //SCRIVO ALL'UTENTE IL PREZZO SCONTATO
+    finalprice.innerText = textmajorprice + calcmajorprice;
 }
 
 
